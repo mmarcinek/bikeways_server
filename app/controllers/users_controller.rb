7dbf04d7a4a903ad44ae15e1924f9832b2c286fc
Michael Marcinek <michaelmarcinek@gmail.com>
2015-12-14 20:41:16 -0500

    basic user model set, sign-in and sign-up processing through devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,7 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  # user_signed_in? helper available
+  # current_user available via Devise
+  # user_session available via Devise
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'users/new'
 
   get 'static_pages/home'
 
