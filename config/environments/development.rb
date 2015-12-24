7c7dd2bee73caf6e35478ed3fc0ccf39ab4b7624
Michael Marcinek <michaelmarcinek@gmail.com>
2015-12-23 19:15:10 -0500

    config SMTP

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   # action_mailer default url
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
