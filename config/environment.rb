665e6f4dd8355a3e5ef4a521f7eed6fc89b8991c
Michael Marcinek <michaelmarcinek@gmail.com>
2015-12-25 15:08:22 -0500

    edits environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "bikeways.io@gmail.com",
-  :password             => "4x5Graflex",
+  :password             => ,
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
